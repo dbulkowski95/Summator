06b707abd17f2d437187abfdcb35af5ec5aeefe2
dbulkowski95 <d.bulkowski95@gmail.com>
2018-11-05 11:11:08 +0100

    U2 and NKD function to add and sub

--- summatorTest.c
+++ summatorTest.c
@@ -4,122 +4,107 @@
 #include "summator.h"
 #include <criterion/criterion.h>
 
-//Test(addNKD, a_equal_b)
-//{
-//	unsigned first = 102;
-//	unsigned second = 102;
-//
-//	printf("%d\n", add(first, second));
-//}
-//
-//Test(addNKD, a_greater_b)
-//{
-//	unsigned first = 129;
-//	unsigned second = 63;
-//
-//	printf("%d\n", add(first, second));
-//}
-//
-//Test(addNKD, a_samller_b)
-//{
-//	unsigned first = 1;
-//	unsigned second = 254;
-//
-//	printf("%d\n", add(first, second));
-//}
-//
-//Test(addNKD, a_and_b_zero)
-//{
-//	unsigned first = 0;
-//	unsigned second = 0;
-//
-//	printf("%d\n", add(first, second));
-//}
-//
-//Test(addNKD, char_error)
-//{
-//	unsigned first = -23;
-//	unsigned second = 10;
-//
-//	printf("%d\n", add(first, second));
-//}
-//
-//Test(addNKD, two_negative_char)
-//{
-//	unsigned first = -23;
-//	unsigned second = -10;
-//
-//	printf("%d\n", add(first, second));
-//}
-//
-//Test(addNKD, hex)
-//{
-//	unsigned first = 0xff;
-//	unsigned second = 0x81;
-//
-//	printf("%d\n", add(first, second));
-//}
-//
-//Test(U2, a_equal_b)
-//{
-//	int first = 102;
-//	int second = 102;
-//
-//	printf("%d\n", U2add(first, second));
-//}
-//
-//Test(U2, a_greater_b)
-//{
-//	int first = 1024;
-//	int second = 102;
-//
-//	printf("%d\n", U2add(first, second));
-//}
-//
-//Test(U2, a_samller_b)
-//{
-//	int first = 2;
-//	int second = 1012;
-//
-//	printf("%d\n", U2add(first, second));
-//}
-//
-//Test(U2, a_and_b_zero)
-//{
-//	int first = 0;
-//	int second = 0;
-//
-//	printf("%d\n", U2add(first, second));
-//}
-//
-//Test(U2, two_negative_char)
-//{
-//	int first = -30;
-//	int second = -1020;
-//
-//	printf("%d\n", U2add(first, second));
-//}
+Test(addNKD, a_equal_b)
+{
+	unsigned first = 102;
+	unsigned second = 102;
+	cr_expect(add(first, second) == first + second, "Expect true");
+}
+
+Test(addNKD, a_greater_b)
+{
+	unsigned first = 129;
+	unsigned second = 63;
+	cr_expect(add(first, second) == first + second, "Expect true");
+}
+
+Test(addNKD, a_samller_b)
+{
+	unsigned first = 1;
+	unsigned second = 254;
+	cr_expect(add(first, second) == first + second, "Expect true");
+}
+
+Test(addNKD, a_and_b_zero)
+{
+	unsigned first = 0;
+	unsigned second = 0;
+	cr_expect(add(first, second) == first + second, "Expect true");
+}
+
+Test(addNKD, char_error)
+{
+	unsigned first = -23;
+	unsigned second = 10;
+	cr_expect(add(first, second) == first + second, "Expect true");
+}
+
+Test(addNKD, two_negative_char)
+{
+	unsigned first = -23;
+	unsigned second = -10;
+	cr_expect(add(first, second) == first + second, "Expect true");
+}
+
+Test(addNKD, hex)
+{
+	unsigned first = 0xff;
+	unsigned second = 0x81;
+	cr_expect(add(first, second) == first + second, "Expect true");
+}
+
+Test(U2, a_equal_b)
+{
+	int first = 102;
+	int second = 102;
+	cr_expect(U2add(first, second) == first + second, "Expect true");
+}
+
+Test(U2, a_greater_b)
+{
+	int first = 1024;
+	int second = 102;
+	cr_expect(U2add(first, second) == first + second, "Expect true");
+}
+
+Test(U2, a_samller_b)
+{
+	int first = 2;
+	int second = 1012;
+	cr_expect(U2add(first, second) == first + second, "Expect true");
+}
+
+Test(U2, a_and_b_zero)
+{
+	int first = 0;
+	int second = 0;
+	cr_expect(U2add(first, second) == first + second, "Expect true");
+}
+
+Test(U2, two_negative_char)
+{
+	int first = -30;
+	int second = -1020;
+	cr_expect(U2add(first, second) == first + second, "Expect true");
+}
 
 Test(subNKD, sub)
 {
 	int first = 10;
 	int second = 7;
-
-	printf("%d\n", sub(first, second));
+	cr_expect(sub(first, second) == first - second, "Expect true");
 }
 
 Test(subU2, sub)
 {
 	int first = -10;
 	int second = -7;
-
-	printf("%d\n", U2sub(first, second));
+	cr_expect(U2sub(first, second) == first - second, "Expect true");
 }
 
 Test(subU2, minus_zero)
 {
 	int first = -0;
-	int second = -7;
-
-	printf("%d\n", U2sub(first, second));
+	int second = -0;
+	cr_expect(U2sub(first, second) == first - second, "Expect true");
 }
