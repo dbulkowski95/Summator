81e6f1d01a1175c314aed3103cb1a34f2480cb62
dbulkowski95 <d.bulkowski95@gmail.com>
2018-11-05 10:53:39 +0100

    U2 and NKD function to add and sub

--- summator.c
+++ summator.c
@@ -2,9 +2,37 @@
 #include <stdio.h>
 #include <limits.h>
 #include "summator.h"
+#include <ctype.h>
 
-unsigned add(unsigned a, unsigned b){
+unsigned add(unsigned a, unsigned b)
+{
 	 if(b == 0)
 		 return a;
 	 return add( a ^ b, (a & b) << 1);
+}
+
+unsigned sub(unsigned x, unsigned y)
+{
+    while (y != 0) {
+        unsigned borrow = (~x) & y;
+        x = x ^ y;
+        y = borrow << 1;
+    }
+    return x;
+}
+
+int U2add(int a, int b){
+	 if(b == 0)
+		 return a;
+	 return U2add( a ^ b, (a & b) << 1);
+}
+
+int U2sub(int a, int b)
+{
+    while (b != 0) {
+        int borrow = (~a) & b;
+        a = a ^ b;
+        b = borrow << 1;
+    }
+    return a;
 }
--- summatorTest.c
+++ summatorTest.c
@@ -4,58 +4,122 @@
 #include "summator.h"
 #include <criterion/criterion.h>
 
-Test(addNKD, a_equal_b)
-{
-	unsigned first = 102;
-	unsigned second = 102;
+//Test(addNKD, a_equal_b)
+//{
+//	unsigned first = 102;
+//	unsigned second = 102;
+//
+//	printf("%d\n", add(first, second));
+//}
+//
+//Test(addNKD, a_greater_b)
+//{
+//	unsigned first = 129;
+//	unsigned second = 63;
+//
+//	printf("%d\n", add(first, second));
+//}
+//
+//Test(addNKD, a_samller_b)
+//{
+//	unsigned first = 1;
+//	unsigned second = 254;
+//
+//	printf("%d\n", add(first, second));
+//}
+//
+//Test(addNKD, a_and_b_zero)
+//{
+//	unsigned first = 0;
+//	unsigned second = 0;
+//
+//	printf("%d\n", add(first, second));
+//}
+//
+//Test(addNKD, char_error)
+//{
+//	unsigned first = -23;
+//	unsigned second = 10;
+//
+//	printf("%d\n", add(first, second));
+//}
+//
+//Test(addNKD, two_negative_char)
+//{
+//	unsigned first = -23;
+//	unsigned second = -10;
+//
+//	printf("%d\n", add(first, second));
+//}
+//
+//Test(addNKD, hex)
+//{
+//	unsigned first = 0xff;
+//	unsigned second = 0x81;
+//
+//	printf("%d\n", add(first, second));
+//}
+//
+//Test(U2, a_equal_b)
+//{
+//	int first = 102;
+//	int second = 102;
+//
+//	printf("%d\n", U2add(first, second));
+//}
+//
+//Test(U2, a_greater_b)
+//{
+//	int first = 1024;
+//	int second = 102;
+//
+//	printf("%d\n", U2add(first, second));
+//}
+//
+//Test(U2, a_samller_b)
+//{
+//	int first = 2;
+//	int second = 1012;
+//
+//	printf("%d\n", U2add(first, second));
+//}
+//
+//Test(U2, a_and_b_zero)
+//{
+//	int first = 0;
+//	int second = 0;
+//
+//	printf("%d\n", U2add(first, second));
+//}
+//
+//Test(U2, two_negative_char)
+//{
+//	int first = -30;
+//	int second = -1020;
+//
+//	printf("%d\n", U2add(first, second));
+//}
 
-	printf("%d\n", add(first, second));
+Test(subNKD, sub)
+{
+	int first = 10;
+	int second = 7;
+
+	printf("%d\n", sub(first, second));
 }
 
-Test(addNKD, a_greater_b)
+Test(subU2, sub)
 {
-	unsigned first = 129;
-	unsigned second = 63;
+	int first = -10;
+	int second = -7;
 
-	printf("%d\n", add(first, second));
+	printf("%d\n", U2sub(first, second));
 }
 
-Test(addNKD, a_samller_b)
+Test(subU2, minus_zero)
 {
-	unsigned first = 1;
-	unsigned second = 254;
+	int first = -0;
+	int second = -7;
 
-	printf("%d\n", add(first, second));
-}
-
-Test(addNKD, a_and_b_zero)
-{
-	unsigned first = 0;
-	unsigned second = 0;
-
-	printf("%d\n", add(first, second));
-}
-
-Test(addNKD, char_error)
-{
-	unsigned first = -23;
-	unsigned second = 10;
-
-	printf("%d\n", add(first, second));
-}
-
-Test(addNKD, two_char_error)
-{
-	unsigned first = -23;
-	unsigned second = -10;
-
-	printf("%d\n", add(first, second));
-}
-
-Test(addNKD, hex)
-{
-	unsigned first = 0xff;
-	unsigned second = 0x81;
-
-	printf("%d\n", add(first, second));
+	printf("%d\n", U2sub(first, second));
 }
