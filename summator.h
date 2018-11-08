c150f8cf87c3b4eefefeab351a7b9662841151f2
dbulkowski95 <d.bulkowski95@gmail.com>
2018-11-08 19:21:22 +0100

    kurwa

--- summator.h
+++ summator.h
@@ -1,6 +1,8 @@
 #ifndef SUMMATOR_H_
 #define SUMMATOR_H_
 
-
-
+unsigned add(unsigned a, unsigned b);
+unsigned sub(unsigned a, unsigned b);
+int U2add(int a, int b);
+int U2sub(int a, int b);
 #endif /* SUMMATOR1_H_ */
