e47cd0467020c657b66f033d9b321e8c43ea1714
dbulkowski95 <d.bulkowski95@gmail.com>
2018-11-09 15:20:28 +0100

    fuck this shit

--- summator.h
+++ summator.h
@@ -1,6 +1,19 @@
 #ifndef SUMMATOR_H_
 #define SUMMATOR_H_
 
+#define BIT(K) (1u << K)
+#define GET_BIT(N,K) ((N & BIT(K)) >> K)
+#define ONE_AND(N) (N & 1)
+#define ONE_LEFT_SHIFT(N) (N << 1)
+#define ONE_RIGHT_SHIFT(N) (N >> 1)
 
+unsigned add(const unsigned a, const unsigned b);
+unsigned sub(const unsigned a, const unsigned b);
+unsigned multiply(unsigned a, unsigned b);
+
+int U2add(const int a, const int b);
+int U2sub(const int a, const int b);
+int itterAdd(const int a, const int b);
+int AddByMacro(int a, int b);
 
 #endif /* SUMMATOR1_H_ */
